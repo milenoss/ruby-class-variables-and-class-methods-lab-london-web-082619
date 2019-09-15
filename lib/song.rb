70d0161726997ae435e68615b9ec395f71b2f94b
Milen Khanal <milenkhanal@gmail.com>
2019-09-15 17:02:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,12 +3,12 @@ require 'set'
 
 class Song 
  attr_reader :name, :artist, :genre
- @@count = []
+ @@count = 0
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
-    @@count = 0
+    @@count = []
   end
   
 end
